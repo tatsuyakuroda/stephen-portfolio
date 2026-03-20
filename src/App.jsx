26e3ecad77db2ffe637dd0ef038bd8bbe4c8ad56
tatsuyakuroda <tatsuyakuroda60@gmail.com>
2026-03-20 13:50:52 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="canvas">
+      <img
+        src="/images/stephen.png"
+        alt="Stephen"
+        className="portrait"
+      />
+    </div>
+  )
+}
+
+export default App
